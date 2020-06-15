f5247c33f564fbbc98c44a0831b66d428daf7692
Allende Aigbedion <ajaigbedion@gmail.com>
2020-06-15 19:01:41 -0400

    7PM revision

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//score counter
 let score = 0;
+var scoreCounter = document.getElementById('score');
+
+//"hole" objects in wack-a-mole game
+var holes = document.getElementById('whack-a-mole');
+
+//"random" hole(s) that the moles will appear in
+var randomMole;
+
+//mole behavior; checks holes and randomly inserts moles in them
+var randomHoleIndeces = Math.random
+var totalHoles = Math.floor()
+
+//event handling
+holes.addEventListener('click', function(evt){
+    console.log(evt.target)
+})
